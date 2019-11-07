18767385c97855667416462d63d58d380cfc1d16
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-11-08 08:19:24 +0900

    Add next.js basic pages

---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,8 @@
+import Document, { DocumentContext } from 'next/document';
+
+export default class extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+}
