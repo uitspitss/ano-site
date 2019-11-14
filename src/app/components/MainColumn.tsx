7745a833bdaca487d7c79d0d25158954e16b6025
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-11-14 23:12:15 +0900

    Update sidebar

--- src/app/components/MainColumn.tsx
+++ src/app/components/MainColumn.tsx
@@ -5,7 +5,7 @@ import Title from './Title';
 import Profile from './Profile';
 import Contact from './Contact';
 import LatestInfo from './LatestInfo';
-// import CharctersPrintedBackground from './CharctersPrintedBackground';
+import CharctersPrintedBackground from './CharctersPrintedBackground';
 import Sidebar from './Sidebar';
 import { User } from '../services/twitter/models/user';
 import { Tweet } from '../services/twitter/models/tweet';
@@ -17,35 +17,36 @@ type Props = {
 };
 
 const MainColumn: FC<Props> = ({ user, timeline, loading }) => (
-  // <CharctersPrintedBackground text={user ? user.name : 'ano-site'}>
-  <Grid column={2} padded relaxed stackable>
-    <Sidebar />
-    <Grid.Row centered>
-      <Grid.Column width={16}>
-        {loading ? (
-          <Title title="This site is Ano Site." />
-        ) : (
-          <Title
-            title={
-              user
-                ? `This site is ${user.name}'s Homepage`
-                : 'This site is Ano Site.'
-            }
-          />
-        )}
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row centered>
-      <Grid.Column width={4}>
-        <Profile user={user} loading={loading} />
-        <Contact user={user} loading={loading} />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <LatestInfo timeline={timeline} loading={loading} />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-  // </CharctersPrintedBackground>
+  <Sidebar>
+    <CharctersPrintedBackground text={user ? user.name : 'ano-site'}>
+      <Grid column={2} padded relaxed stackable>
+        <Grid.Row centered>
+          <Grid.Column width={16}>
+            {loading ? (
+              <Title title="This site is Ano Site." />
+            ) : (
+              <Title
+                title={
+                  user
+                    ? `This site is ${user.name}'s Homepage`
+                    : 'This site is Ano Site.'
+                }
+              />
+            )}
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Grid.Column width={4}>
+            <Profile user={user} loading={loading} />
+            <Contact user={user} loading={loading} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <LatestInfo timeline={timeline} loading={loading} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </CharctersPrintedBackground>
+  </Sidebar>
 );
 
 export default MainColumn;
