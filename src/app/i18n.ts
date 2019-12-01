dbdcc5a4e6c47aa87e9eb9f001d517d7c70a5068
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-12-01 14:19:33 +0900

    Update

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -22,9 +22,8 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    fallbackLng: 'ja',
-    debug: true,
-
+    fallbackLng: 'en',
+    debug: false,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
