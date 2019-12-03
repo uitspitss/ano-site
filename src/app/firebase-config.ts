2572041bf5812cbee2d09496b6deb7b0006869b8
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-12-04 03:35:47 +0900

    Update firebase-config

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.APP_API_KEY,
+  authDomain: process.env.APP_AUTH_DOMAIN,
+  databaseURL: process.env.APP_DATABASE_URL,
+  projectId: process.env.APP_PROJECT_ID,
+  storageBucket: process.env.APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.APP_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
