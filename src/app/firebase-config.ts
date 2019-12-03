fd038630305e6216dfd80e0198431ca704726e06
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-12-04 03:25:23 +0900

    Fix

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,6 +1,6 @@
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
