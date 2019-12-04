c7d5849c7a9e6867cd85c6ff567572664f901357
uitspitss <3781626+uitspitss@users.noreply.github.com>
2019-12-04 13:56:20 +0900

    Replace authDomain of env with original value

--- src/app/firebase-config.ts
+++ src/app/firebase-config.ts
@@ -1,6 +1,6 @@
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: 'ano-site.firebaseapp.com',
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
